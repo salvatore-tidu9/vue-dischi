ba6a6d825dd7b6c42cb0850cddfa834f6dd11285
Salvatore Tidu <salvatoret9@outlook.it>
2021-03-30 22:39:11 +0200

    Vue_Dischi_(Bonus_1 + Bonus_2)

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ var app = new Vue({
 
         discsList: [],
 
+        genres: [],
+
+        selectedGenre: ""
+
     },
 
     mounted() {
@@ -17,6 +21,29 @@ var app = new Vue({
         .then((result) => {
 
             this.discsList= result.data.response;
-        })
+
+            //-------------------------------------------
+
+            this.discsList.forEach(disc => {
+
+                if (!this.genres.includes(disc.genre)) {
+
+                    this.genres.push(disc.genre);
+
+                }
+
+            });
+
+            //-------------------------------------------
+
+            this.discsList.sort(function(a, b) {
+
+                return (a.year) - (b.year);
+
+            });
+
+        });
+
     }
+
 });
